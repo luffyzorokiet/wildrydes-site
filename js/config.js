27dd60c9181d9d38d286baa5a626151e456a667f
luffyzorokiet <49666113+luffyzorokiet@users.noreply.github.com>
2026-02-09 18:26:39 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_J3sBTAMxc', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '54mhtppqdg187o7lespp3qrif7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_QPX8LaX4z', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4q8n07f97mdt0uaqelud06tj2u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
